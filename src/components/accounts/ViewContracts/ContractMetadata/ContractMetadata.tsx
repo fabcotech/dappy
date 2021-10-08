8424b997944e41476fc13ed29978785a84bf8472
Fabco <contact@fabco.tech>
2021-10-08 14:08:56 +0200

    ContractMetadata always receives valid config as parameter. Added loader

--- src/components/accounts/ViewContracts/ContractMetadata/ContractMetadata.tsx
+++ src/components/accounts/ViewContracts/ContractMetadata/ContractMetadata.tsx
@@ -1,35 +1,35 @@
-import React from "react";
+import React from 'react';
 
 import { RChainContractConfig } from '/models';
 import { feePermillage, toDuration, toDurationString } from '/utils/unit';
 
 interface ContractMetadataProps {
-  contractConfig?: RChainContractConfig
+  contractConfig: RChainContractConfig;
 }
 
-const ContractMetadataComponent = ({ contractConfig } : ContractMetadataProps) => {
-  return (<div className="mb-5">
-  {contractConfig && (
-    <span 
-      className={`${contractConfig.locked ? 'has-text-success' : 'has-text-danger'}`}
-      title={contractConfig.locked ? t('locked title'): t('not locked title')}>
-      <i className={`mr-1 fa fa-${contractConfig.locked ? 'lock' : 'exclamation-triangle'}`}></i>
-      {t(contractConfig.locked ? 'locked' : 'not locked')}
-    </span>
-  )}
-  {contractConfig && contractConfig.expires && (
-    <span className="ml-2">
-      <i className="fa fa-clock mx-1"></i>
-      {t('expiration')}: {toDurationString(t, toDuration(contractConfig.expires))}
-    </span>
-  )}
-  {contractConfig && contractConfig.fee && (
-    <span className="ml-2">
-      <i className="fa fa-money-bill-wave mr-1"></i>
-      {t('rchain token fee')}: {feePermillage(contractConfig.fee)}%
-    </span>
-  )}
-  </div>);
-}
+const ContractMetadataComponent = ({ contractConfig }: ContractMetadataProps) => {
+  return (
+    <div className="mb-5">
+      <span
+        className={`${contractConfig.locked ? 'has-text-success' : 'has-text-danger'}`}
+        title={contractConfig.locked ? t('locked title') : t('not locked title')}>
+        <i className={`mr-1 fa fa-${contractConfig.locked ? 'lock' : 'exclamation-triangle'}`}></i>
+        {t(contractConfig.locked ? 'locked' : 'not locked')}
+      </span>
+      {contractConfig.expires && (
+        <span className="ml-2">
+          <i className="fa fa-clock mx-1"></i>
+          {t('expiration')}: {toDurationString(t, toDuration(contractConfig.expires))}
+        </span>
+      )}
+      {contractConfig.fee && (
+        <span className="ml-2">
+          <i className="fa fa-money-bill-wave mr-1"></i>
+          {t('rchain token fee')}: {feePermillage(contractConfig.fee)}%
+        </span>
+      )}
+    </div>
+  );
+};
 
 export const ContractMetadata = ContractMetadataComponent;
--- src/components/accounts/ViewContracts/ViewContracts.tsx
+++ src/components/accounts/ViewContracts/ViewContracts.tsx
@@ -22,38 +22,53 @@ async function getPursesAndContractConfig({
   pursesIds,
   masterRegistryUri,
   contractId,
-  version
+  version,
 }: {
-  blockchain: Blockchain,
-  pursesIds: string[],
-  masterRegistryUri: string,
-  contractId: string,
-  version: string
+  blockchain: Blockchain;
+  pursesIds: string[];
+  masterRegistryUri: string;
+  contractId: string;
+  version: string;
 }): Promise<[RequestResult<RChainTokenPurse>, RequestResult<RChainContractConfig>]> {
   const indexes = blockchain.nodes.filter((n) => n.readyState === 1).map(getNodeIndex);
 
-  return multiCallParseAndValidate([
-    { 
-      execute: () => readPursesTerm({masterRegistryUri, contractId, pursesIds}), 
-      parse: parseRhoValToJs, 
-      validate: validate(rchainTokenValidators[version].purses)
-    },
-    { 
-      execute: () => readConfigTerm({masterRegistryUri, contractId}), 
-      parse: parseRhoValToJs, 
-      validate: validate(rchainTokenValidators[version].contractConfig)
+  return multiCallParseAndValidate(
+    [
+      {
+        execute: () => readPursesTerm({ masterRegistryUri, contractId, pursesIds }),
+        parse: parseRhoValToJs,
+        validate: validate(rchainTokenValidators[version].purses),
+      },
+      {
+        execute: () => readConfigTerm({ masterRegistryUri, contractId }),
+        parse: parseRhoValToJs,
+        validate: validate(rchainTokenValidators[version].contractConfig),
+      },
+    ],
+    {
+      chainId: blockchain.chainId,
+      urls: indexes,
+      resolverMode: 'absolute',
+      resolverAccuracy: 100,
+      resolverAbsolute: indexes.length,
+      multiCallId: fromBlockchain.EXPLORE_DEPLOY_X,
     }
-  ],
-  {
-    chainId: blockchain.chainId,
-    urls: indexes,
-    resolverMode: 'absolute',
-    resolverAccuracy: 100,
-    resolverAbsolute: indexes.length,
-    multiCallId: fromBlockchain.EXPLORE_DEPLOY_X,
-  }) as Promise<[RequestResult<RChainTokenPurse>, RequestResult<RChainContractConfig>]>;
+  ) as Promise<[RequestResult<RChainTokenPurse>, RequestResult<RChainContractConfig>]>;
 }
 
+const Loading = (props: { contractId: string }) => {
+  return (
+    <Fragment>
+      <div className="address-and-copy fc">
+        <span className="address">
+          {t('contract') + ' '}
+          {props.contractId}
+        </span>
+        <i className="fa fa-after fa-redo rotating"></i>
+      </div>
+    </Fragment>
+  );
+};
 interface ViewContractsProps {
   namesBlockchain?: Blockchain;
   contractId: string;
@@ -116,7 +131,7 @@ export class ViewContractsComponent extends React.Component<ViewContractsProps, 
       pursesIds: this.props.pursesIds.slice(0, 100),
       masterRegistryUri: this.props.rchainInfos.info.rchainNamesMasterRegistryUri,
       contractId: this.props.contractId,
-      version: this.props.version
+      version: this.props.version,
     });
 
     const validationErrors = [pursesRequest.validationErrors, contractConfigRequest.validationErrors]
@@ -143,60 +158,57 @@ export class ViewContractsComponent extends React.Component<ViewContractsProps, 
         </div>
       );
     }
-    return (
-      <Fragment>
-        <div className="address-and-copy fc">
-          <span className="address">
-            {t('contract') + ' '}
-            {this.state.contractConfig?.fungible && (
-              <span title="Contract for fungible tokens">
-                FT
-              </span>
-            )}
-            {!this.state.contractConfig?.fungible && (
-              <span title="Contract for non-fungible tokens">
-                NFT
-              </span>
-            )}
-            {this.props.contractId}
-          </span>
+    if (this.state.contractConfig) {
+      const contractConfig = this.state.contractConfig as RChainContractConfig;
+      return (
+        <Fragment>
+          <div className="address-and-copy fc">
+            <span className="address">
+              {t('contract') + ' '}
+              {contractConfig.fungible && <span title="Contract for fungible tokens">FT</span>}
+              {!contractConfig.fungible && <span title="Contract for non-fungible tokens">NFT</span>}
+              {this.props.contractId}
+            </span>
 
-          <span className="square ml5" style={{ background: toRGB(this.props.contractId) }}></span>
-          <a type="button" className="underlined-link" onClick={() => window.copyToClipboard(this.props.contractId)}>
-            <i className="fa fa-copy fa-before"></i>
-            {t('copy contract address')}
-          </a>
-        </div>
-        <ContractMetadata contractConfig={this.state.contractConfig} />
-        {this.props.pursesIds.length > 100 && (
-          <div className="x-by-100-purses">Purses 100 / {this.props.pursesIds.length}</div>
-        )}
-        <div className="view-purses">
-          {this.props.pursesIds.length === 0 ? (
-            <div>
-              <span className="no-purses">No purses</span>
-            </div>
-          ) : undefined}
-          {this.props.pursesIds.slice(0, 100).map((id) => {
-            return (
-              <ViewPurse
-                rchainInfos={this.props.rchainInfos}
-                account={this.props.account}
-                key={id}
-                id={id}
-                contractId={this.props.contractId}
-                fungible={this.state.contractConfig?.fungible}
-                privateKey={this.props.privateKey}
-                purse={this.state.purses[id]}
-                sendRChainTransaction={this.props.sendRChainTransaction}
-                contractExpiration={this.state.contractConfig?.expires}
-              />
-            );
-          })}
-          {this.props.pursesIds.length > 100 && <div className="more-purses">...</div>}
-        </div>
-      </Fragment>
-    );
+            <span className="square ml5" style={{ background: toRGB(this.props.contractId) }}></span>
+            <a type="button" className="underlined-link" onClick={() => window.copyToClipboard(this.props.contractId)}>
+              <i className="fa fa-copy fa-before"></i>
+              {t('copy contract address')}
+            </a>
+          </div>
+          <ContractMetadata contractConfig={contractConfig} />
+          {this.props.pursesIds.length > 100 && (
+            <div className="x-by-100-purses">Purses 100 / {this.props.pursesIds.length}</div>
+          )}
+          <div className="view-purses">
+            {this.props.pursesIds.length === 0 ? (
+              <div>
+                <span className="no-purses">No purses</span>
+              </div>
+            ) : undefined}
+            {this.props.pursesIds.slice(0, 100).map((id) => {
+              return (
+                <ViewPurse
+                  rchainInfos={this.props.rchainInfos}
+                  account={this.props.account}
+                  key={id}
+                  id={id}
+                  contractId={this.props.contractId}
+                  fungible={contractConfig.fungible}
+                  privateKey={this.props.privateKey}
+                  purse={this.state.purses[id]}
+                  sendRChainTransaction={this.props.sendRChainTransaction}
+                  contractExpiration={contractConfig.expires}
+                />
+              );
+            })}
+            {this.props.pursesIds.length > 100 && <div className="more-purses">...</div>}
+          </div>
+        </Fragment>
+      );
+    } else {
+      return <Loading contractId={this.props.contractId}></Loading>;
+    }
   }
 }
 
