f3ab59a01248e7af9fcc0aec8a41953edbc3745b
Paul Musso <paul@musso.io>
2021-11-22 09:16:52 +0100

    ci: be sure to set electron version to the latest

---
+++ outdated.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DEPENDENCIES=$1
+FAIL=false
+
+for DEP in $(echo $DEPENDENCIES | sed "s/,/ /g")
+do
+    TO_BE_PARSED=$(npm outdated -p $DEP)
+    ACTUAL=$(echo "$TO_BE_PARSED" | cut -d: -f3)
+    ACTUAL_VERSION=$(echo "$ACTUAL" | cut -d@ -f2)
+    LATEST=$(echo "$TO_BE_PARSED" | cut -d: -f4)
+    LATEST_VERSION=$(echo "$LATEST" | cut -d@ -f2)
+    if [ "$ACTUAL" != "$LATEST" ]; then
+      echo "SECURITY ISSUE $DEP: actual $ACTUAL_VERSION, latest $LATEST_VERSION, run npm i $DEP:latest to fix it"
+      FAIL=true
+    fi
+done
+
+if [[ "$FAIL" == true ]]; then
+  exit 1
+fi
