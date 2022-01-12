aa9ae2b342211980b7c605c6eeb3177859fa2f2d
Fabco <contact@fabco.tech>
2022-01-12 15:50:50 +0100

    removal of ethereum-util library, just copied 20 lines of code instead

--- src/utils/wallets/evmWallet.ts
+++ src/utils/wallets/evmWallet.ts
@@ -1,8 +1,24 @@
 import { Wallet } from './wallet';
 
 import { TxData, Transaction, JsonTx } from '@ethereumjs/tx';
-import { fromUtf8 } from 'ethereumjs-util'
 import { utils } from 'rchain-toolkit';
+
+// copied from ethereumjs-util lib
+const padToEven = (value: string) => {
+  let a = value;
+  if (typeof a !== 'string') {
+      throw new Error(`[padToEven] value must be type 'string', received ${typeof a}`);
+  }
+  if (a.length % 2)
+      a = `0${a}`;
+  return a;
+}
+
+// copied from ethereumjs-util lib
+const fromUtf8 = (stringValue: string) => {
+  const str = Buffer.from(stringValue, 'utf8');
+  return `0x${padToEven(str.toString('hex')).replace(/^0+|0+$/g, '')}`;
+}
 
 export const evmWallet: Wallet<TxData, JsonTx> = {
   signTransaction: (tx, privateKey) => {
